7552df254617a2e84a310fe604b805fb89ad4dab
qanglee <545540710@qq.com>
2022-04-04 20:01:33 +0800

    test

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { mongodbConf } = require('../config/index')
 const {dbName,host,port} = mongodbConf;
-const db = `mongodb://play:play@${host}:${port}` //mongoose 地址
+const db = `mongodb://admin:123456@${host}:${port}` //mongoose 地址
 mongoose.connect(db,{
     useNewUrlParser:true,
     dbName:dbName
